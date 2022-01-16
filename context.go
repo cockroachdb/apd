23eba28ff49810ed9d7f838719fe3d556630b338
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2022-01-16 04:59:49 -0500

    apd: inline Context.round
    This commit reworks the call to `Context.round` to allow for mid-stack
    function inlining. To do this, we remove the conditionally call to one
    of two functions, which is considered too complex to inline.

--- context.go
+++ context.go
@@ -1220,8 +1220,8 @@ func (c *Context) quantize(d, v *Decimal, exp int32) Condition {
 			// target eliminates this problem.
 
 			d.Exponent = -diff
-			// Avoid the c.Precision == 0 check.
-			res = nc.Rounding.Round(nc, d, d)
+			// Round even if nc.Precision == 0.
+			res = nc.Rounding.Round(nc, d, d, false /* disableIfPrecisionZero */)
 			// Adjust for 0.9 -> 1.0 rollover.
 			if d.Exponent > 0 {
 				d.Coeff.Mul(&d.Coeff, bigTen)
